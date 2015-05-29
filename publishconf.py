38c89ef75d190ed3ab20283da6675523180a18df
Manish-Fueled <manish@fueled.co>
2015-05-29 16:20:06 +0530

    updated site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://ahujamanish.github.io'
+SITEURL = 'http://ahujamanish.github.io/ios-tips'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
