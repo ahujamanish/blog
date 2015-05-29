b6f11ff6c21f52b21976e78f0ea35fd551309d0b
Manish-Fueled <manish@fueled.co>
2015-05-29 15:25:01 +0530

    initial source dump

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Manish Ahuja'
+SITENAME = u'Digital Dairy'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('linkedIn', 'https://www.linkedin.com/profile/view?id=51979793&trk=nav_responsive_tab_profile'),
+         ('facebook', 'https://www.facebook.com/its.manish.ahuja'))
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
